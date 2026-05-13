90564d5fb13f1a2137e431b27bb06fea288f975f
ThaoThaoHeart <jennyhoang_9@hotmail.com>
2026-05-13 15:58:11 -0400

    Dashboard

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-	path('', views.save_profile, name='save_profile')
+	path('', views.dashboard, name='dashboard'),
+	path('saveprofile/', views.save_profile, name='save_profile'),
+	path('<int:id>/', views.resume, name='resume')
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,4 +1,26 @@
 from django.shortcuts import render
+from .models import Profile
+
+def dashboard(request):
+	resumes = Profile.objects.all()
+	return render(request, 'myapp/dashboard.html', {'resumes':resumes})
 
 def save_profile(request):
+	if request.method == 'POST':
+		name = request.POST.get('name')
+		email = request.POST.get('email')
+		phone = request.POST.get('phone')
+		degree = request.POST.get('degree')
+		school = request.POST.get('school')
+		university = request.POST.get('university')
+		summary = request.POST.get('summary')
+		previous_work = request.POST.get('previous_work')
+		skills = request.POST.get('skills')
+		profile = Profile(name=name,email=email,phone=phone,degree=degree,school=school,university=university,summary=summary,previous_work=previous_work,skills=skills)
+		profile.save()
+
 	return render(request, 'myapp/accept.html')
+
+def resume(request,id):
+	user_profile = Profile.objects.get(id=id)
+	return render(request,'myapp/resume.html',{'user_profile':user_profile})
